bafaf697394e500f3ae8be14ec2948219fc4bb75
Daniel Mansilla <dmansilla@previred.com>
2018-05-07 11:55:10 -0300

    cambio de nombre

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'potato',
-  packages = ['potato'], # this must be the same as the name above
+  name = 'autopotato',
+  packages = ['autopotato'], # this must be the same as the name above
   version = '0.1',
   description = 'potato driver',
   author = 'Daniel Mansilla',
   author_email = 'daanichannel@gmail.com',
   url = 'https://github.com/Mansilla1/potato', # use the URL to the github repo
   download_url = 'https://github.com/Mansilla1/potato/tarball/0.1',
-  keywords = ['testing', 'logging', 'example'],
+  keywords = ['testing', 'automate', 'selenium'],
   classifiers = [],
 )
